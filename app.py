387c2d30c00025846456acd56dc8e37da1ea6405
AndyZhuAZ <andyzhuaz@outlook.com>
2020-04-24 11:01:06 +0800

    tip file and api

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ import os
 from config import Config
 from forms import LoginForm
 # from models import User
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
@@ -21,6 +21,7 @@ login.login_view = 'login'
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 @app.route('/')
 @app.route('/index')
@@ -38,19 +39,20 @@ def editor():
 @login_required
 def upload():
     data = json.loads(request.get_data())
-    filename = data['text'].strip().split('\n')[0][1:]
-    print(filename)
-    print(data)
+    filename = data['text'].split('\n')[0][1:].strip()
+    # print(filename)
+    # print(data)
     try:
         with open('static/markdown/{}/{}.md'.format(data['tip_type'], filename), 'w') as f:
             f.write(data['text'])
+            print('Save a file named {} in path {}'.format(filename, data['tip_type']))
     except IOError:
         return '上传失败：文件 写入/读取 失败'
     else:
         return '文件上传成功'
 
 
-@app.route('/getfile/<tip_type>')
+@app.route('/api/getfile/<tip_type>')
 def get_file(tip_type):
     dic = {
         'tip_type': tip_type,
@@ -66,23 +68,34 @@ def get_file(tip_type):
     return res
 
 
-@app.route('/tips/<tip_type>/<name>')
+@app.route('/api/tips/<tip_type>/<name>')
 def tips(tip_type, name):
-    with open('static/markdown/{}/{}'.format(tip_type, name), 'r') as f:
-        return f.read()
+    dic = {
+        'title': name,
+        'type': tip_type,
+        'content': None
+    }
+    try:
+        with open('static/markdown/{}/{}'.format(tip_type, name), 'r') as f:
+            dic['content'] = f.read()
+    except IOError:
+        return '文件读取失败，检查文件名和路径'
+    else:
+        res = json.dumps(dic)
+        return res
 
 
-@app.route('/login',methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username = form.username.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('用户名或密码无效')
             return redirect(url_for('login'))
-        login_user(user,remember=form.remember_me.data)
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -112,7 +125,8 @@ class tip():
     tip_type = ''
     content = ''
 
-class User(UserMixin,db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -121,13 +135,11 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def set_password(self,password):
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
-
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 if __name__ == '__main__':
